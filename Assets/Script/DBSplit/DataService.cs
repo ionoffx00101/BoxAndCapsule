efd9ed8d4cc4bed0425df817da71d9b3f492c428
Nora <anes344@naver.com>
2017-04-11 18:18:57 +0900

    17.04.11

---
+++ Assets/Script/DBSplit/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class DataService : MonoBehaviour {
+
+    //public IDbConnection getDBconn (String DatabaseName)
+    //   {
+    //       //var dbPath = string.Format(@"Assets/StreamingAssets/{0}" , DatabaseName);
+    //       string conn = "URI=file:" + Application.dataPath + "/"+DatabaseName+".db";
+
+    //       IDbConnection dbconn;
+    //       dbconn = (IDbConnection) new SqliteConnection(conn);
+
+    //       return dbconn;
+    //   }
+
+    private String DatabaseName = "Capsule";
+
+    public IDbConnection getDBconn()
+    {
+        //var dbPath = string.Format(@"Assets/StreamingAssets/{0}" , DatabaseName);
+        string conn = "URI=file:" + Application.dataPath + "/" + DatabaseName + ".db";
+
+        IDbConnection dbconn;
+        dbconn = (IDbConnection) new SqliteConnection(conn);
+
+        return dbconn;
+    }
+
+}
---
+++ Assets/Script/DBSplit/SelectCapsule.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+public class SelectCapsule : MonoBehaviour {
+
+    DataService ds;
+    //
+    private IDbConnection dbconn;
+    
+    void Start () {
+
+        ds = new DataService();
+        //
+        dbconn = ds.getDBconn();
+        dbconn.Open();
+
+        // IDbCommand
+        IDbCommand dbcmd = dbconn.CreateCommand();
+        // sql문장 = "SELECT 조회할 컬럼 FROM 조회할 테이블";
+        string sqlQuery = "SELECT value,name " + "FROM Capsule";
+        // dbcmd.Connection = dbconn; 위에서 쓰임
+        dbcmd.CommandText = sqlQuery;
+
+        // IDataReader
+        IDataReader reader = dbcmd.ExecuteReader();
+        while ( reader.Read() )
+        {
+            // 변수타입은 컬럼 데이터 타입에 맞추면 된다.
+            int value = reader.GetInt32(0);
+            string name = reader.GetString(1);
+
+            Debug.Log("value= " + value + "  name =" + name);
+        }
+
+        // 닫아주고 초기화 시켜주는 곳
+        reader.Close();
+        reader = null;
+        dbcmd.Dispose();
+        dbcmd = null;
+        dbconn.Close();
+        dbconn = null;
+    }
+}
